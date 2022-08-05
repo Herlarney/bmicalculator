c291f79793a7f447a561dc9695a25ab96350d642
Herlarney <sradecoration@gmail.com>
2022-08-05 08:50:29 +0100

    modification

--- lib/iconcontent.dart
+++ lib/iconcontent.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: use_full_hex_values_for_flutter_colors, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const labelTextStyle = TextStyle(fontSize: 15, color: Color(0x0FFF8D8E98));
 
--- lib/inputpage.dart
+++ lib/inputpage.dart
@@ -1,12 +1,12 @@
-// ignore_for_file: prefer_const_constructors, duplicate_ignore, unused_import, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, duplicate_ignore, unused_import, prefer_const_literals_to_create_immutables, avoid_print
 
 import 'package:flutter/material.dart';
 import 'iconcontent.dart';
 import 'ReusableCard.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-const activecardColor = Color(0xFF1D1E33);
-const inactivecardColor = Color(0x0FF111328);
+const activeCardColor = Color(0xFF1D1E33);
+const inactiveCardColor = Color(0xFF111328);
 
 class BMICaculator extends StatefulWidget {
   const BMICaculator({Key? key}) : super(key: key);
@@ -16,6 +16,19 @@ class BMICaculator extends StatefulWidget {
 }
 
 class _BMICaculatorState extends State<BMICaculator> {
+  Color maleCardColor = inactiveCardColor;
+  Color femalecardColor = inactiveCardColor;
+  //1=male 2=female
+  void updateColor(int gender) {
+    if (gender == 1) {
+      if (maleCardColor == inactiveCardColor) {
+        maleCardColor = activeCardColor;
+      } else {
+        maleCardColor = inactiveCardColor;
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -32,23 +45,35 @@ class _BMICaculatorState extends State<BMICaculator> {
             Row(
               children: <Widget>[
                 Expanded(
-                    child: ReusableCard(
-                  cardChild: IconContent(
-                    icon: FontAwesomeIcons.mars,
-                    label: 'Male',
-                  ),
-                  // ignore: prefer_const_constructors
-                  color: activecardColor,
-                  width: 180,
-                )),
-                Expanded(
+                    child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      updateColor(1);
+                    });
+                  },
                   child: ReusableCard(
                     cardChild: IconContent(
-                      icon: FontAwesomeIcons.venus,
-                      label: 'Female',
+                      icon: FontAwesomeIcons.mars,
+                      label: 'Male',
                     ),
-                    color: activecardColor,
+                    // ignore: prefer_const_constructors
+                    color: maleCardColor,
                     width: 180,
+                  ),
+                )),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      print('female button got pressed');
+                    },
+                    child: ReusableCard(
+                      cardChild: IconContent(
+                        icon: FontAwesomeIcons.venus,
+                        label: 'Female',
+                      ),
+                      color: femalecardColor,
+                      width: 180,
+                    ),
                   ),
                 ),
               ],
@@ -56,17 +81,17 @@ class _BMICaculatorState extends State<BMICaculator> {
             Expanded(
                 child: ReusableCard(
               // ignore: prefer_const_constructors
-              color: activecardColor,
+              color: activeCardColor,
               width: double.infinity,
               height: double.infinity,
             )),
             Row(
               children: <Widget>[
                 Expanded(
-                    child: ReusableCard(color: activecardColor, width: 180)),
+                    child: ReusableCard(color: activeCardColor, width: 180)),
                 Expanded(
                     child: ReusableCard(
-                  color: activecardColor,
+                  color: activeCardColor,
                   width: 180,
                 )),
               ],
